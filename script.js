4566fe132be72a5f20a5974ab89fed1493e26334
Shahnoorishtiyaque <148809310+Shahnoorishtiyaque@users.noreply.github.com>
2025-06-15 03:49:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 let toggleBtn = document.getElementById("themeIcon");
+let WA=document.getElementById("WA");
+let call=document.getElementById("call");
+let linkedin=document.getElementById("linkedin");
+let mail=document.getElementById("mail");
 
 toggleBtn.onclick = function () {
     document.body.classList.toggle("dark");
-
     // Swap icon based on current mode
     if (document.body.classList.contains("dark")) {
         toggleBtn.src = "images/sun.svg";
+        WA.src="images/wawhite.svg";
+        call.src="images/phone-callwhite.svg";
+        linkedin.src="images/linkedinwhite.svg";
+        mail.src="images/mailwhite.svg";
+        
     } else {
         toggleBtn.src = "images/moon.svg";
+         WA.src="images/whatsapp-icon.svg";
+        call.src="images/phone-call.svg";
+        linkedin.src="images/linkedin.svg";
+        mail.src="images/mail.svg";
+        
     }
 };
